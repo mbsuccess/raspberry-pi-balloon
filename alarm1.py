ac89f9b9dbf6c8afe94c1ee39d5d43abe7ef3879
mike <michael@byingtonit.com>
2024-09-17 20:41:56 -0500

    Added alerm script 3

--- alarm1.py
+++ alarm1.py
@@ -3,22 +3,22 @@ import time
 
 # Setup GPIO
 GPIO.setmode(GPIO.BCM)  # Use BCM pin numbering
-gpio_pin = 23           # GPIO23
+gpio_pin = 25           # GPIO25
 
 # Setup GPIO pin as output
 GPIO.setup(gpio_pin, GPIO.OUT)
 
 try:
-    # Turn GPIO23 ON (HIGH)
+    # Turn ALARM GPIO ON (HIGH)
     GPIO.output(gpio_pin, GPIO.HIGH)
-    print("GPIO23 is ON")
+    print("GPIO" + str(gpio_pin) +" is ON")
     
     # Wait for 0.1 seconds
-    time.sleep(3)
+    time.sleep(0.1)
     
     # Turn GPIO23 OFF (LOW)
     GPIO.output(gpio_pin, GPIO.LOW)
-    print("GPIO23 is OFF")
+    print("GPIO" + str(gpio_pin) +" is OFF")
 
 finally:
     # Clean up GPIO settings
